bb72b47e1efa9ecf18b203498339e85f7e9b5b97
MENasri23 <m.e26159487@gmail.com>
2022-05-16 20:55:45 -0700

    Declare model processor and its properties

--- mapper-processor/src/main/java/ir/jatlin/mapper/processor/ModelMapperProcessor.kt
+++ mapper-processor/src/main/java/ir/jatlin/mapper/processor/ModelMapperProcessor.kt
@@ -1,4 +1,19 @@
 package ir.jatlin.mapper.processor
 
-class ModelMapperProcessor {
+import com.google.devtools.ksp.processing.CodeGenerator
+import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.processing.Resolver
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.symbol.KSAnnotated
+
+class ModelMapperProcessor(
+    private val codeGenerator: CodeGenerator,
+    private val logger: KSPLogger,
+    private val options: Map<String, String>
+) : SymbolProcessor {
+
+    override fun process(resolver: Resolver): List<KSAnnotated> {
+        // implement
+        return emptyList()
+    }
 }
